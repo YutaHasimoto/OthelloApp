b619b9ef56a3d15b8bb2c79894cce5fec934d80f
Yuta Hashimoto <modular.07.lacquer@icloud.com>
2024-08-31 21:06:45 +0900

    Initial Commit

---
+++ OthelloApp/OthelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OthelloAppApp.swift
+//  OthelloApp
+//
+//  Created by 橋本雄太 on 2024/08/31.
+//
+
+import SwiftUI
+
+@main
+struct OthelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
